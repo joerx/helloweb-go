83ac82986ec6bd3fd37e867793154ca34b3decba
Joerg Henning <henning.joerg@gmail.com>
2018-05-01 14:22:16 +0800

    Report hostname

--- pkg/handlers/handlers.go
+++ pkg/handlers/handlers.go
@@ -4,21 +4,35 @@ import (
 	"html/template"
 	"log"
 	"net/http"
+	"os"
 )
 
 // IndexPageData represents the data to be rendered by the index page template
 type IndexPageData struct {
-	Message string
-	Title   string
+	Message  string
+	Title    string
+	Hostname string
 }
 
 // HandleIndex renders the index page for the web server
 func HandleIndex(w http.ResponseWriter, r *http.Request) {
-	data := &IndexPageData{Message: "Hello World", Title: "Index Page"}
+
+	hostname, err := os.Hostname()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	data := &IndexPageData{
+		Message:  "Hello World",
+		Title:    "Index Page",
+		Hostname: hostname,
+	}
+
 	tpl, err := template.ParseFiles("templates/index.html")
 	if err != nil {
 		log.Fatal(err)
 	}
+
 	if err := tpl.Execute(w, data); err != nil {
 		log.Fatal(err)
 	}
